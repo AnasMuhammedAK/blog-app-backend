09cef270c89c366bb7eb3eb49a90a2c83d1dd8ca
AnasMuhammedAK <anasabdulkareem100@gmail.com>
2022-10-01 19:27:34 +0530

    deployment

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ app.use(errorHandler)
 
 
 //Server configuration 
-const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`Server running on port ${PORT}`))
+app.listen(process.env.PORT || 5000,console.log(`Server running on port ${PORT}`))
 
 
 
